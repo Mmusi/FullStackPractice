8409e80ce94fd2f962ced378fdad28ea36ef1e7e
Thato Mmusi <mmusi2010@gmail.com>
2021-11-25 16:27:21 +0200

    update

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Content from './components/Content'
+import Filter from './components/Filter'
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setAllCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    if (newFilter) {
+      const regex = new RegExp( newFilter, 'i' );
+      const filteredCountries = () => allCountries.filter(country => country.name.match(regex))
+      setCountries(filteredCountries)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Filter value={newFilter} onChange={handleFilterChange} />
+      <Content countries={countries} setCountries={setCountries} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
